b6614858461a98c0448ac5c2ac5222546dfedc7e
Edward Ofosu Mensah <edward.mensah@ashesi.edu.gh>
2024-08-19 05:59:35 +0000

    JS for rock paper scissors game

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -29,6 +29,28 @@ function autoplay()
     }
     
 }
+
+document.querySelector(".js-rock-button").addEventListener("click", ()=>{
+    playGame("rock");
+} );
+
+document.querySelector(".js-paper-button").addEventListener("click", () =>{
+    playGame("paper");
+});
+
+document.querySelector(".js-scissors-button").addEventListener("click", () =>{
+    playGame("scissors");
+});
+document.body.addEventListener("keydown", (event) =>{
+    if (event.key==="r"){
+        playGame("rock");
+    }else if (event.key==="s"){
+        playGame("scissors");
+    }else if (event.key==="p"){
+        playGame("paper")
+    }
+
+});
 function playGame(playerMove)
 {
 let result = '';
